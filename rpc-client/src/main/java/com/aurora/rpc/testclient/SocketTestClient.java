113d4b63d1ccbc052a56d7aeb0c3ac6cc002784f
liuchang <451519704@qq.com>
2021-02-17 01:01:40 +0800

    [v1.0] 支持多个服务的注册

--- rpc-client/src/main/java/com/aurora/rpc/testclient/SocketTestClient.java
+++ rpc-client/src/main/java/com/aurora/rpc/testclient/SocketTestClient.java
@@ -1,6 +1,8 @@
 package com.aurora.rpc.testclient;
 
 import com.aurora.rpc.RpcClientProxy;
+import com.aurora.rpc.api.ByeObject;
+import com.aurora.rpc.api.ByeService;
 import com.aurora.rpc.api.HelloObject;
 import com.aurora.rpc.api.HelloService;
 import com.aurora.rpc.socket.client.SocketClient;
@@ -10,9 +12,15 @@ public class SocketTestClient {
     public static void main(String[] args) {
         SocketClient client = new SocketClient("127.0.0.1", 9000);
         RpcClientProxy proxy = new RpcClientProxy(client);
+
         HelloService helloService = proxy.getProxy(HelloService.class);
-        HelloObject object = new HelloObject(12, "This is a message");
-        String res = helloService.hello(object);
-        System.out.println(res);
+        HelloObject helloObject = new HelloObject(12, "This is a hello message");
+        ByeService byeService = proxy.getProxy(ByeService.class);
+        ByeObject byeObject = new ByeObject(16, "This is a bye message");
+
+        String res1 = helloService.hello(helloObject);
+        String res2 = byeService.bye(byeObject);
+        System.out.println(res1);
+        System.out.println(res2);
     }
 }
--- rpc-server/src/main/java/com/aurora/rpc/testserver/SocketTestServer.java
+++ rpc-server/src/main/java/com/aurora/rpc/testserver/SocketTestServer.java
@@ -1,6 +1,7 @@
 package com.aurora.rpc.testserver;
 
 import com.aurora.rpc.RpcServer;
+import com.aurora.rpc.api.ByeService;
 import com.aurora.rpc.api.HelloService;
 import com.aurora.rpc.registry.DefaultServiceRegistry;
 import com.aurora.rpc.registry.ServiceRegistry;
@@ -14,8 +15,10 @@ import com.aurora.rpc.socket.server.SocketServer;
 public class SocketTestServer {
     public static void main(String[] args) {
         HelloService helloService = new HelloServiceImpl();
+        ByeServiceImpl byeService = new ByeServiceImpl();
         ServiceRegistry serviceRegistry = new DefaultServiceRegistry();
         serviceRegistry.register(helloService);
+        serviceRegistry.register(byeService);
         SocketServer socketServer = new SocketServer(serviceRegistry);
         socketServer.start(9000);
     }
